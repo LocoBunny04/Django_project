ede332784bf1df954f5964e6528f99727c87df43
Haase <78894109+LocoBunny04@users.noreply.github.com>
2025-07-18 12:29:44 -0500

    Simple Form

--- djsite/cybr/views.py
+++ djsite/cybr/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Task
+from .forms import CreateNewList
 
 # Create your views here.
 def home(response):
@@ -14,7 +15,19 @@ def home(response):
 	return render(response, "cybr/home.html", {"name": "test"})
 
 def create(response):
-    return render(response, "cybr/create.html", {})
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+
+        return HttpResponseRedirect("/%i" %t.id)
+
+    else:
+        form = CreateNewList()
+    return render(response, "cybr/create.html", {"form":form})
 
 # def index_nm(response, name):
 #     ls = ToDoList.objects.get(name=name)
